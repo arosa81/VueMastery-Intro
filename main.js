ea7df5abc7668120f7aab651e95c4ea543512f59
Alexander Rosa <alex.j.rosa@gmail.com>
2020-02-29 11:30:15 -0700

    applied conditional rendering

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-//add a link to data object
-//use v-bind to bind link to an anchor tag's href attribute
+//add an onSale property to data object
+//use v-if to display a span that says "On Sale!" whenever onSale is true
 var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
     description: 'Christmas socks with the Vue brand',
     image: './assets/vmSocks-green-onWhite.jpg',
-    linkedImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+    linkedImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+    inStock: true,
+    inventory: 100,
+    onSale: true,
+    onSaleMessage: 'On Sale!'
   }
 })
