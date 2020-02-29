628720a64ded2dfe4c6f59e71721413580f1bc95
Alexander Rosa <alex.j.rosa@gmail.com>
2020-02-29 11:12:07 -0700

    applied v-binds for image and links

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-//add a description to data object
-//use expression in html to display description beneth H1 value
+//add a link to data object
+//use v-bind to bind link to an anchor tag's href attribute
 var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: "Christmas socks with the Vue brand"
+    description: 'Christmas socks with the Vue brand',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    linkedImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
   }
 })
