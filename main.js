0d0f31d530fcec3e982d667ed38ca2248d9cd92c
Alexander Rosa <alex.j.rosa@gmail.com>
2020-02-29 10:35:37 -0700

    added description data and expression

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+//add a description to data object
+//use expression in html to display description beneth H1 value
 var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
+    description: "Christmas socks with the Vue brand"
   }
 })
